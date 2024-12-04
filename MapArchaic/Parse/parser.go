58bf7cf970885a60dbabf4dd04726f19359b060e
moltapy <2263423153@qq.com>
2024-12-04 12:24:20 +0800

    add exe name as help

--- MapArchaic/Parse/parser.go
+++ MapArchaic/Parse/parser.go
@@ -2,7 +2,9 @@ package parse
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
 )
 
 type Args struct {
@@ -26,22 +28,31 @@ func (args *Args) Parse() {
 	args.OutFile = flag.String("out", "", "Mapped score file path")
 	args.ReadDepth = flag.String("depth", "false", "Show read depth in result file(optional), bool, true for showing, false for not showing")
 	flag.Parse()
-
-	if *args.BedMode != "" && *args.MskFile == "" {
-		log.Fatal("If you use the prama BED, The prama MASK FILE cannot be nil, Please check!")
-	} else if *args.MskFile != "" && *args.BedMode == "" {
-		log.Println("If you use the prama MASK FILE, and may expect that it works, You may indicate include/exclude the mask file in the prama BED")
-	} else if *args.BedMode != "include" && *args.BedMode != "exclude" && *args.BedMode != "" {
-		log.Fatal("The prama BED only has three options, include/exclude or nil(default),Please check!")
-	} else if *args.ScoreFile == "" {
-		log.Fatal("The prama SCORE FILE cannot be nil, Please check!")
-	} else if *args.RefTag == "" {
-		log.Fatal("The prama REFERENCE TAG cannot be nil, Please check!")
-	} else if *args.ReadDepth != "true" && *args.ReadDepth != "false" {
-		log.Fatal("The depth received is just true/false, Please check!")
-	} else if *args.OutFile == "" {
-		log.Fatal("The Prama OUTPUT FILE cannot be nil, Please check!")
-	} else {
-		log.Default()
+	if len(os.Args) > 2 {
+		if *args.BedMode != "" && *args.MskFile == "" {
+			log.Fatal("If you use the prama BED, The prama MASK FILE cannot be nil, Please check!")
+		} else if *args.MskFile != "" && *args.BedMode == "" {
+			log.Println("If you use the prama MASK FILE, and may expect that it works, You may indicate include/exclude the mask file in the prama BED")
+		} else if *args.BedMode != "include" && *args.BedMode != "exclude" && *args.BedMode != "" {
+			log.Fatal("The prama BED only has three options, include/exclude or nil(default),Please check!")
+		} else if *args.ScoreFile == "" {
+			log.Fatal("The prama SCORE FILE cannot be nil, Please check!")
+		} else if *args.RefTag == "" {
+			log.Fatal("The prama REFERENCE TAG cannot be nil, Please check!")
+		} else if *args.ReadDepth != "true" && *args.ReadDepth != "false" {
+			log.Fatal("The depth received is just true/false, Please check!")
+		} else if *args.OutFile == "" {
+			log.Fatal("The Prama OUTPUT FILE cannot be nil, Please check!")
+		} else {
+			log.Default()
+		}
+	} else if len(os.Args) == 1 {
+		exePath, err := os.Executable()
+		if err != nil {
+			log.Fatal("Problem occurred when getting abspath, Please report this bug!")
+		}
+		fmt.Printf("Usage of %s:\n", exePath)
+		flag.PrintDefaults()
+		os.Exit(1)
 	}
 }
