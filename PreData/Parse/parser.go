d740cfc3ef8550070cf2625e8e3943924dc51bd1
moltapy <2263423153@qq.com>
2024-12-04 22:00:19 +0800

    update

--- PreData/Parse/parser.go
+++ PreData/Parse/parser.go
@@ -5,7 +5,6 @@ import "flag"
 type Args struct {
 	WorkPath   *string
 	SampleFile *string
-	//SubgroupFile *string
 	OutGroup   *string
 	ModernFile *string
 	SepChar    *string
@@ -14,7 +13,6 @@ type Args struct {
 func (args *Args) Parse() {
 	args.WorkPath = flag.String("wkp", "", "WorkPath of the whole project")
 	args.SampleFile = flag.String("smp", "", "Sample file path")
-	//args.SubgroupFile = flag.String("sbg", "", "Generated subgroup file")
 	args.OutGroup = flag.String("oug", "", "Outgroup pop name")
 	args.ModernFile = flag.String("mdv", "", "modern vcf file")
 	args.SepChar = flag.String("sep", "\t", "separation")
