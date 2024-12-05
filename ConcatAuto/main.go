10bd31d2332535b752999e9942b94410768132a4
moltapy <2263423153@qq.com>
2024-12-05 22:30:25 +0800

    Update main.go

--- ConcatAuto/main.go
+++ ConcatAuto/main.go
@@ -2,16 +2,15 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	parse "gosprime/ConcatAuto/Parse"
 	"io"
 	"log"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 	"sync"
-	"fmt"
-	"strconv"
-	"bytes"
 )
 
 var args = parse.Args{}
@@ -35,14 +34,14 @@ func readPops(path string) []string {
 	var popGroups []string
 	popList, err := os.Open(path)
 	if err != nil {
-		log.Fatal("Problem occurred when open population list file, Please check!", err)
+		log.Fatalf("Problem occurred when open population list file, Please check!\nERROR:%s\n", err)
 	}
 	reader := bufio.NewReader(popList)
 	for {
 		lineBytes, err := reader.ReadBytes('\n')
 		line := strings.TrimRight(string(lineBytes), "\r\n")
 		if err != nil && err != io.EOF {
-			log.Fatal("Problem occurred when read from reader",err)
+			log.Fatalf("Problem occurred when read from reader\nERROR:%s\n", err)
 		}
 		if line != "" {
 			popGroups = append(popGroups, line)
@@ -60,23 +59,23 @@ func concatAutos(pop string) {
 	var pramaList = []string{"concat"}
 	pathPrefix := *args.WorkPath + "/" + pop + "/"
 	for chrom := 1; chrom <= 22; chrom++ {
-		pramaList = append(pramaList, pathPrefix + strings.Replace(*args.VcfName, "{chrom}", strconv.Itoa(chrom), 1))
+		pramaList = append(pramaList, pathPrefix+strings.Replace(*args.VcfName, "{chrom}", strconv.Itoa(chrom), 1))
 	}
-	pramaList = append(pramaList,"--naive-force","--output-type","z","--output",pathPrefix + *args.ConcatedFile)
-	fmt.Println(pramaList)
+	pramaList = append(pramaList, "--naive-force", "--output-type", "z", "--output", pathPrefix+*args.ConcatedFile)
 	concatCommand := exec.Command(*args.BcfTool, pramaList...)
-	
+
 	var stderr bytes.Buffer
 	concatCommand.Stderr = &stderr
-	
+
+	log.Printf("Bcftools concat VCF files in %s start!\n", pop)
 	if err := concatCommand.Start(); err != nil {
-		log.Fatal("Problem occurred when start concatCommand",err)
+		log.Fatal("Problem occurred when start concatCommand", err)
 	}
 
 	if err := concatCommand.Wait(); err != nil {
-		log.Fatalf("Problem occurred when wait concatCommand,err=%v,stderr :%s",err,stderr.String())
+		log.Fatalf("Problem occurred when wait concatCommand,err=%v,stderr :%s", err, stderr.String())
 	}
 
-	log.Println("Success!")
+	log.Printf("Bcftools concat success in %s!\n", pop)
 
 }
