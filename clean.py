3fdac6460daf08ad7a2a483ad6496d64f50c49a1
moltapy <2263423153@qq.com>
2025-05-02 13:43:59 +0800

    # 增加快速清理生成文件的脚本，方便调试，快速清除生成文件和错误生成的列

--- clean.py
+++ clean.py
@@ -1,6 +1,19 @@
+import yaml
 import shutil
+import argparse
 
-def main():
+
+def clean_total():
+
+    paths = yaml.load(open("config.yaml", "rb"), yaml.FullLoader)["paths"]
+    shutil.rmtree(paths["splited_samplelists_dirpath"])
+    shutil.rmtree(paths["outgroup_samplelists_dirpath"])
+    shutil.rmtree(paths["modernhuman_extracted_vcffiles_dirpath"])
+    shutil.rmtree(paths["concated_vcffiles_dirpath"])
+    shutil.rmtree(paths["generated_scorefiles_dirpath"])
+    
+    
+def clean_mapping():
 
     for index in range(1, 23):
         path = f"score_groups/CDX/CDX_YRI_{index}.score"
@@ -13,5 +26,20 @@ def main():
                 outfile.write(line+"\n")
             outfile.close()
         shutil.move(movepath, path)
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Cleaner of the workflow")
+    parser.add_argument('-m','--mapping', action="store_true", help="Just clean mapping columns")
+    parser.add_argument('-a', '--all', action="store_true", help = "Clean all workflow outputs")
+    args = parser.parse_args()
+
+    if args.mapping:
+        clean_mapping()
+    if args.all:
+        clean_total()
+
+
 if __name__ == "__main__":
     main()
